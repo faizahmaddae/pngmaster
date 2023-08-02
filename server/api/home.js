898556f613d09c09c9e25f4279c29ca1e1b7610a
faizodev <139255818+faizodev@users.noreply.github.com>
2023-08-02 11:34:07 -0400

    new update

--- server/api/home.js
+++ server/api/home.js
@@ -1,38 +1,30 @@
 import * as cheerio from 'cheerio';
 import axios from 'axios';
-
+// playwright
+import { chromium } from 'playwright';
+import { firefox } from 'playwright';
+import { log } from 'console';
 
 export default defineEventHandler(async (event) => {
 
     // const { name } = getQuery(event)
     const query = getQuery(event)
 
-    const headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        // 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        // 'Accept-Encoding': 'gzip, deflate',
-        // 'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-        // 'Cache-Control': 'max-age=0',
-        // 'Connection': 'keep-alive',
-        // 'Host': 'www.pngwing.com',
-        // 'Referer': 'https://www.pngwing.com/',
-        // 'Upgrade-Insecure-Requests': '1',
-        // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-      };
-
-    const bodyHTML = await axios.get('https://www.pngwing.com/', { headers });
-
-    return {
-        statusCode: 200,
-        body:`${bodyHTML.statusText}!`,
-    };
+    const bodyHTML = await axios.get(`https://www.pngwing.com/`,{
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Accept': 'application/json, text/plain, */*',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Accept-Language': 'en-US,en;q=0.9',
+          'Connection': 'keep-alive',
+          'Host': 'pngwing.com',
+          'Origin': 'https://pngwing.app',
+          'Referer': 'https://pngwing.app/',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+        },
+      })
 
     const $ = cheerio.load(bodyHTML.data);
-
-    // return {
-    //     statusCode: 200,
-    //     body: bodyHTML.data,
-    // };
 
     // find ul with id list_ul
     const items = $('.hotsearch > ul').find('li');
@@ -51,44 +43,46 @@ export default defineEventHandler(async (event) => {
             atext: atext,
         };
         arr.push(item);
-
+    
     });
 
 
-    // releated list_ul
-    const list_ul = $('#list_ul').find('li');
-    var list_ul_data = [];
-    // loop through the items
-    list_ul.each((i, el) => {
+      // releated list_ul
+      const list_ul = $('#list_ul').find('li');
+      var list_ul_data = [];
+      // loop through the items
+      list_ul.each((i, el) => {
+  
+          const link = $(el).find('link[itemprop="contentUrl"]').attr('href');
+          const keywords = $(el).find('meta[itemprop="keywords"]').attr('content');
+          const figcaption = $(el).find('figcaption').text();
+          const thumbnail = $(el).find('a[itemprop="url"]').attr('src');
+          const permalink = $(el).find('a[itemprop="url"]').attr('href');
+          // https://www.pngwing.com/en/free-png-nueth
+          // https://www.pngwing.com/en/{slug}
+  
+          // get slug from permalink use regex
+          const slug = (permalink + "").replaceAll('https://www.pngwing.com/en/', '');
+  
+          // if link is empty, skip
+          if (!link) return;
+  
+          // create new object
+          const item = {
+              link: link,
+              thumbnail: thumbnail,
+              keywords: keywords,
+              figcaption: figcaption,
+              permalink: permalink,
+              slug: slug,
+              // slug: slug,
+          };
+  
+          list_ul_data.push(item);
+  
+      });
 
-        const link = $(el).find('link[itemprop="contentUrl"]').attr('href');
-        const keywords = $(el).find('meta[itemprop="keywords"]').attr('content');
-        const figcaption = $(el).find('figcaption').text();
-        const thumbnail = $(el).find('a[itemprop="url"]').attr('src');
-        const permalink = $(el).find('a[itemprop="url"]').attr('href');
-        // https://www.pngwing.com/en/free-png-nueth
-        // https://www.pngwing.com/en/{slug}
-
-        // get slug from permalink use regex
-        const slug = (permalink + "").replaceAll('https://www.pngwing.com/en/', '');
-
-        // if link is empty, skip
-        if (!link) return;
-
-        // create new object
-        const item = {
-            link: link,
-            thumbnail: thumbnail,
-            keywords: keywords,
-            figcaption: figcaption,
-            permalink: permalink,
-            slug: slug,
-            // slug: slug,
-        };
-
-        list_ul_data.push(item);
-
-    });
+    //   arr.push(list_ul_data);
 
     return {
         statusCode: 200,
