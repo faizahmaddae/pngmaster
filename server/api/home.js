2d0f20701fb3bf0f2dbfa81a6fe1fb8206a29563
faizodev <139255818+faizodev@users.noreply.github.com>
2023-08-02 12:17:03 -0400

    axios

--- server/api/home.js
+++ server/api/home.js
@@ -12,26 +12,29 @@ export default defineEventHandler(async (event) => {
     // const { name } = getQuery(event)
     const query = getQuery(event)
 
-
-    var mydata = [];
-
-    await gotScraping.get('https://www.pngwing.com/').then((response) => {
-        // console.log(response.body);
-
-        // load the response into cheerio
-
-        mydata = response.body;
-
+    const response = await axios.get('https://www.pngwing.com/en/', {
+        headers: {
+            "headers": {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Host": "pngwing.com",
+                "Sec-Ch-Ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"",
+                "Sec-Ch-Ua-Mobile": "?0",
+                "Sec-Ch-Ua-Platform": "\"macOS\"",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "none",
+                "Sec-Fetch-User": "?1",
+                "Upgrade-Insecure-Requests": "1",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+                "X-Amzn-Trace-Id": "Root=1-630ce94f-289bc1c678d8cd7153e42f5a"
+            }
+        },
     });
 
-    return {
-        statusCode: 200,
-        body: mydata,
-    };
 
-
-
-    const $ = cheerio.load(response.body);
+    const $ = cheerio.load(response.data);
 
     // find ul with id list_ul
     const items = $('.hotsearch > ul').find('li');
